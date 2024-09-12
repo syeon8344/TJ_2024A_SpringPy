64660bcbf1052329a843a89c306ca9dffb161ceb
syeon8344 <yeon8344@gmail.com>
2024-09-12 18:04:43 +0900

    0912 PM

--- src/main/java/web/chat/ChatController.java
+++ src/main/java/web/chat/ChatController.java
@@ -2,6 +2,7 @@ package web.chat;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
@@ -14,7 +15,7 @@ public class ChatController extends TextWebSocketHandler {
     // - 클라이언소켓들의 접속명단을 저장하는 컬렉션 프레임워크 // ArrayList(비동기)  vs Vector(동기)
     private List< WebSocketSession > currentClientSocketList = new Vector<>();
 
-    // 1. 클라이언트가 서버 웹소켓에 접속 요청했을때
+    // 1. 클라이언트가 서버 웹소켓에 접속 요청하고 연결된 후 (after established)
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         System.out.println("session = " + session);
@@ -25,7 +26,7 @@ public class ChatController extends TextWebSocketHandler {
         System.out.println("서버소켓의 접속 인원 : " + currentClientSocketList.size());
     }
 
-    // 2. 클라이언트가 서버 웹소켓에 접속을 끊으면
+    // 2. 클라이언트가 서버 웹소켓에 접속을 끊은 후 (after closed)
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         System.out.println("session = " + session);
@@ -34,5 +35,25 @@ public class ChatController extends TextWebSocketHandler {
         currentClientSocketList.remove(session);
         // - 현재 접속된 인원수
         System.out.println("서버소켓의 접속 인원 : " + currentClientSocketList.size());
+        // - 다른 클라이언트 소켓들에게 나간 멤버를 알림
+        for (WebSocketSession clientSession : currentClientSocketList){
+            // 목록에 저장된 각 세션을 호출해서 메시지 전송
+            TextMessage textMessage = new TextMessage("Hello, ClientSocket");
+            handleTextMessage(null, textMessage);
+        }
+    }
+
+    // 3. 클라이언트가 서버 소켓에 메시지를 보냈을 때
+    // 서버가 메시지를 받을 때 이후 로직 구현
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        System.out.println("session = " + session + ", message = " + message);
+        System.out.println(message.getPayload());  // TextMessage payload=[Hello, Ser..],
+            // 메시지 내용 DB처리? -> service > dao DB처리
+        // - 특정 세션에서 받은 메시지를 접속된 다른 클라이언트 소켓들에게도 전달
+        for (WebSocketSession clientSession : currentClientSocketList){
+            // 목록에 저장된 각 세션을 호출해서 메시지 전송
+            clientSession.sendMessage(message);
+        }
     }
 }
--- src/main/resources/static/js/chat/chat.js
+++ src/main/resources/static/js/chat/chat.js
@@ -1,3 +1,74 @@
 console.log("chat")
+// 비회원제 채팅, 익명 식별이름 생성 e.g. 익명1100
+// Math.random() = 0~1 사이 실수, Math.floor() = 소수점 아래 버리기 -> 1 ~ 곱한 값까지의 정수
+let nickname = `익명${Math.floor(Math.random()*10000) + 1}`
 // JS 클라이언트 웹소켓
 let clientSocket = new WebSocket("ws://localhost:8080/chat/conn")  // ip:port/웹소켓 매핑 주소
+console.log(clientSocket);
+
+// [1] clientSocket의 onclose, onerror, onopen, onmessage 오버라이드 필요
+// 1) socket.onopen: 서버 소켓과 접속 성공시 실행되는 함수를 대입
+clientSocket.onopen = (e) => {
+    console.log("Connected to ServerSocket!")
+    clientSocket.send(JSON.stringify({type: "join", nickname: nickname, date: new Date().toLocaleString()}))
+}
+
+// 2) socket.onmessage: 서버 소켓에서 메시지를 보내올 때 실행되는 함수를 대입
+clientSocket.onmessage = (e) => {
+    console.log(e);
+    console.log(e.data);
+    let msgBox = document.querySelector("#msgBox")
+    // JSON 형식의 문자열이 응답이므로 타입 변환
+    let data = JSON.parse(e.data)
+    if (data.type == "join"){
+        // join 타입: 새 멤버 참여, 안내 메시지 출력
+        msgBox.innerHTML += `<div class="info">
+                                <span>[${data.date.split(" ")[3]} ${data.date.split(" ")[4]}]</span> 
+                                <span>(${data.nickname}님이 참여하셨습니다.)</span>
+                            </div>`
+    }
+    else if (data.type == "leave"){
+        msgBox.innerHTML += `<div class="info">
+                                <span>[${data.date.split(" ")[3]} ${data.date.split(" ")[4]}]</span> 
+                                <span>(${data.nickname}님이 나가셨습니다.)</span>
+                            </div>`
+    }
+    else if (data.nickname == nickname){
+        // 자신이 보낸 메시지일 경우 날짜를 왼쪽에
+        msgBox.innerHTML += `<div class="msgTo">
+                                <div>${data.nickname}</div>
+                                <div>
+                                    <span>[${data.date.split(" ")[3]} ${data.date.split(" ")[4]}]</span> 
+                                    <span>${data.message}</span>
+                                </div>
+                            </div>`
+    } else {
+        // 다른 사람의 메시지일 경우 날짜를 오른쪽에 
+        msgBox.innerHTML += `<div class="msgFrom">
+                                <div>${data.nickname}</div>
+                                <div>
+                                    <span>${data.message}</span>
+                                    <span>[${data.date.split(" ")[3]} ${data.date.split(" ")[4]}]</span> 
+                                </div>
+                            </div>`
+    }
+}
+
+// [2] 메시지 전송 이벤트
+function onMsgSend(){
+    // - 현재 클라이언트 소켓과 연결 유지된 서버 소켓에게 메시지 전달
+    msgInput = document.querySelector("#msgInputBox")
+    // 닉네임과 메시지를 포매팅해 전송
+    let message = {
+        'type':'msg',
+        'nickname': nickname,
+        'message': msgInput.value,
+        'date': new Date().toLocaleString()
+    }
+    clientSocket.send(JSON.stringify(message))
+    // [object Object] vs "{\"nickname\":\"익명1172\",\"message\":\"aaaaaaaaa\",\"date\":\"2024. 9. 12. 오후 12:17:56\"}"
+    // HTTP에서는 contentType: application/json
+    // @GetMapping()에서는 자동으로 JSON -> DTO
+    // WebSocket에서는 JSON.stringify()로 문자열 타입인 JSON 형식으로 변환해서 전송해야 한다.
+    // {key: value}: 객체 타입 JSON 형식, "{key: value}": 문자열 타입 JSON 형식
+}
